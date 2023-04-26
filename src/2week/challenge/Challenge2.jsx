f8bc493254c37b5557dbd4eef2654edc814db02d
cookie <topseon23@fnfcorp.com>
2023-04-26 19:01:20 +0900

    일단 답은 맞춤

--- src/2week/challenge/Challenge2.jsx
+++ src/2week/challenge/Challenge2.jsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+
 export function getFinalState(baseState, queue) {
-  let finalState = baseState;
+  let finalState = baseState
 
   // TODO: do something with the queue...
+  queue.map((action) => {
+    typeof action === 'function' ? finalState = action(finalState) : finalState = action
+  })
+  
+  // state를 써서 해보고 싶었는데,, 잘 안되네요ㅠ
+  // const [finalState, setFinalState] = useState(baseState)
+
+  // useEffect(()=>{
+  //   queue.map((action) => {
+  //     typeof action === 'function'? setFinalState(n => action(n)):setFinalState(action)
+  //     console.log(finalState)
+  //   })
+  // }, [])
 
   return finalState;
 }
